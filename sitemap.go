dec3c1893c01e7978c4242677bab18d87ca87840
roywang <i@roy.wang>
2024-09-25 18:50:36 +0800

    修复sitemap无法被读取的问题

--- sitemap.go
+++ sitemap.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/xml"
+	"fmt"
 	"os"
 	"time"
 )
@@ -17,15 +18,14 @@ type URL struct {
 	ChangeFreq string `xml:"changefreq"`
 }
 
-// generateSitemap 更新以包含主页和 changefreq 标签
 func generateSitemap(posts []PostMetadata, blogconfigs *BlogConfig, outputPath string) error {
 	urlSet := URLSet{}
 
 	// 包含主页
 	homeUrl := URL{
 		Loc:        blogconfigs.URI,
-		LastMod:    time.Now().Format(time.RFC3339), // 或者用你博客的最近更新时间
-		ChangeFreq: "always",                        // 或者根据你的更新频率：daily, weekly, monthly 等
+		LastMod:    time.Now().Format(time.RFC3339),
+		ChangeFreq: "always",
 	}
 	urlSet.Urls = append(urlSet.Urls, homeUrl)
 
@@ -34,21 +34,26 @@ func generateSitemap(posts []PostMetadata, blogconfigs *BlogConfig, outputPath s
 		url := URL{
 			Loc:        blogconfigs.URI + "/" + post.URI + "/",
 			LastMod:    post.Date,
-			ChangeFreq: "weekly", // 假设博客文章不经常变化，可以根据实际情况调整
+			ChangeFreq: "weekly",
 		}
 		urlSet.Urls = append(urlSet.Urls, url)
 	}
 
 	file, err := os.Create(outputPath)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to create output file: %w", err)
 	}
 	defer file.Close()
+
+	// 写入 XML 声明
+	if _, err := file.WriteString(`<?xml version="1.0" encoding="UTF-8"?>` + "\n"); err != nil {
+		return fmt.Errorf("failed to write XML declaration: %w", err)
+	}
 
 	encoder := xml.NewEncoder(file)
 	encoder.Indent("", "  ")
 	if err := encoder.Encode(urlSet); err != nil {
-		return err
+		return fmt.Errorf("failed to encode sitemap: %w", err)
 	}
 
 	return nil
